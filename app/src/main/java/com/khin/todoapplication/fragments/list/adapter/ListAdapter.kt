10a8cefdfb611a5fd99c8e71bfe4b85b73e803a4
Khin <khinthirisoe.mdy@gmail.com>
2021-01-31 23:18:25 +0700

    implement ToDoDiffUtil

--- app/src/main/java/com/khin/todoapplication/fragments/list/adapter/ListAdapter.kt
+++ app/src/main/java/com/khin/todoapplication/fragments/list/adapter/ListAdapter.kt
@@ -2,6 +2,7 @@ package com.khin.todoapplication.fragments.list.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.khin.todoapplication.data.models.ToDoData
 import com.khin.todoapplication.databinding.RowLayoutBinding
@@ -20,8 +21,10 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
     }
 
     fun setData(toDoData: List<ToDoData>) {
+        val toDoDiffUtil = ToDoDiffUtil(dataList, toDoData)
+        val toDoDiffResult = DiffUtil.calculateDiff(toDoDiffUtil)
         this.dataList = toDoData
-        notifyDataSetChanged()
+        toDoDiffResult.dispatchUpdatesTo(this)
     }
 
     override fun getItemCount(): Int = dataList.size
